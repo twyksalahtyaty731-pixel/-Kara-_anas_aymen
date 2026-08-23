6e7a29550fd26f97344ecd17bd35a6c2efc4a012
twyksalahtyaty731-pixel <twyksalahtyaty731@gmail.com>
2026-08-23 07:18:51 +0200

    Update اون.js

--- script/commands/developer/اون.js
+++ script/commands/developer/اون.js
@@ -3,8 +3,8 @@ const fs = require("fs-extra");
 module.exports.config = {
   name: "بوت",
   version: "1.0.0",
-  hasPermssion: 2, // للمطور (أيمن) فقط
-  credits: "أيمن",
+  hasPermssion: 2, // للمطور(تويكس) فقط
+  credits: "تويكس",
   description: "تشغيل أو إيقاف استجابة البوت للعامة عبر وضع المطور",
   commandCategory: "الـمطـور",
   usages: "اون / اوف",
@@ -31,6 +31,6 @@ module.exports.run = async function({ api, event, args }) {
   } 
   
   else {
-    return api.sendMessage(`⌬ ━━ 𝗞𝗜𝗥𝗔 ━━ ⌬\n\nالوضع الحالي: ${config.DeveloperMode ? "مُعطّل (اوف)" : "يعمل (اون)"}\nاستخدم:\n.بوت اون\n.بوت اوف`, threadID, messageID);
+    return api.sendMessage(`⌬ ━━ MIRA ━━ ⌬\n\nالوضع الحالي: ${config.DeveloperMode ? "مُعطّل (اوف)" : "يعمل (اون)"}\nاستخدم:\n.بوت اون\n.بوت اوف`, threadID, messageID);
   }
 };
